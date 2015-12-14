16651596e162d0aa7d60f175926120ecb2c4eb93
Bryan Kok <bryan.wyern1@gmail.com>
2015-12-15 04:41:40 +0800

    .gitignore is now working

--- build/lib.linux-x86_64-2.7/algolib/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'Bryan Kok'
-
-from config import config
-from populate_db import create_db
-
-from query_db import query_db
--- build/lib.linux-x86_64-2.7/algolib/config.py
+++
@@ -1,3 +0,0 @@
-config = {
-    "img_types": ['.JPEG', '.JPG', '.jpg', '.png', '.jpeg', '.bmp', '.dcx', '.gif', '.pcx', '.ppm', '.psd', '.tga', '.tif', '.tiff', '.xpm']
-}
