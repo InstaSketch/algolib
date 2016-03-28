55829963314160975562f1f8f0996f52a9f93a70
Dylan Wang <wanghaoyu@frazil.me>
2016-03-29 07:32:39 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='algolib',
     version='0.3',
-    packages=['algolib'],
+    packages=['algolib', 'algolib.descriptors'],
     url='https://github.com/InstaSketch/algolib',
     license='',
     author='Bryan Kok, Dylan Wang',
